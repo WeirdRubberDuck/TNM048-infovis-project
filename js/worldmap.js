15909632f5bb486a2b94d0d860054e1e68532a89
Emma Broman <emmbr740@student.liu.se>
2019-03-04 10:35:50 +0100

    Add tooltip on map hover

--- js/worldmap.js
+++ js/worldmap.js
@@ -1,14 +1,19 @@
 /**
- * TODO: write description
+ * Function to create an interactive world map to visualise a value (given by "key")
+ * 
+ * Based on an example by Micah Stubbs: http://bl.ocks.org/micahstubbs/8e15870eb432a21f0bc4d3d527b2d14f  
  * 
  * @Created Mar 1, 2019
  * @LastUpdate ...
  * @author Emma Broman & Ingela Rossing
  */
 
-function worldMap(data, key){
+function worldMap(data){
 
     console.log("Create world map...");
+
+    var key_score = "Happiness Score"; // OBS! TODO: Rename in file...
+    var key_rank = "Happiness Rank"; // OBS! TODO: Rename in file...
 
     //Set width and height of the chart
     var div = '#world-map-chart';
@@ -30,12 +35,30 @@ function worldMap(data, key){
         .domain([1.0,2.0,3.0,4.0,5.0,6.0,7.0])
         .range(["rgb(247,251,255)", "rgb(222,235,247)", "rgb(198,219,239)", "rgb(158,202,225)", "rgb(107,174,214)", "rgb(66,146,198)","rgb(33,113,181)"]);
     // TODO: Use a better color scale. Preferably using color brewer
+    var colorUndefined ="rgb(200,200,210)";
 
     var projection = d3.geoMercator()
                        .scale(130)
                        .translate( [width / 2, height / 1.5]);
     
     var path = d3.geoPath().projection(projection);
+
+    // To format happiness score :)
+    var format = d3.format(".3n");
+
+    // Function for setting tooltips
+    var tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .offset([-10, 0])
+        .html(function(d) {
+            return "<strong><span class='details'>" + d.properties.name + "</span></strong>" 
+                + "<br>"
+                + "Happiness Score: <span class='details'>"  + format(d[key_score]) +"</span>"
+                + "<br>"
+                + "Rank: " + "<span class='details'>" + d[key_rank] + "</span>" ;
+        })
+
+    svg.call(tip);
 
     // Load map data and draw the map
     // ------------------------------
@@ -45,9 +68,17 @@ function worldMap(data, key){
 
     function drawMap(error, mapData){
         var happynessPerCountry = {}; 
+        var rankPerCountry = {}; 
 
-        data.forEach(function(d) { happynessPerCountry[d.Country] = +d[key]; });
-        mapData.features.forEach(function(d) { d[key] = happynessPerCountry[d.properties.name] });
+        // Set values for each country
+        data.forEach(function(d) { 
+            happynessPerCountry[d.Country] = +d[key_score]; 
+            rankPerCountry[d.Country] = +d[key_rank]; 
+        });
+        mapData.features.forEach(function(d) { 
+            d[key_score] = happynessPerCountry[d.properties.name] 
+            d[key_rank] = rankPerCountry[d.properties.name] 
+        });
 
         svg.append("g")
             .attr("class", "countries")
@@ -57,12 +88,31 @@ function worldMap(data, key){
             .attr("d", path)
             .style("fill", function(d) { 
                 var fillColor = color(happynessPerCountry[d.properties.name]);
-                if(!fillColor) fillColor = "rgb(200,200,210)";
+                if(!fillColor) fillColor = colorUndefined;
                 return fillColor;
             })
             .style('stroke', 'white')
             .style('stroke-width', 1.0)
             .style("opacity",0.8)
+            // tooltips
+            .style("stroke","white")
+            .style('stroke-width', 0.3)
+            .on('mouseover',function(d){
+                tip.show(d);
+
+                d3.select(this)
+                    .style("opacity", 1)
+                    .style("stroke","white")
+                    .style("stroke-width",3);
+            })
+            .on('mouseout', function(d){
+                tip.hide(d);
+
+                d3.select(this)
+                    .style("opacity", 0.8)
+                    .style("stroke","white")
+                    .style("stroke-width",0.3);
+            });
 
         svg.append("path")
             .datum(topojson.mesh(mapData.features, function(a, b) { return a.id !== b.id; }))
